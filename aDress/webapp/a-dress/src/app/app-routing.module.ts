b9dde629e87e705d2228bc4650ac9fe36e2d813a
Vitor <vfrd00@ua.pt>
2022-05-25 00:11:17 +0100

    homepage and navbar componentization

--- aDress/webapp/a-dress/src/app/app-routing.module.ts
+++ aDress/webapp/a-dress/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product/product.component';
 import {StoreComponent} from './store/store.component';
 
 const routes: Routes = [
-  {path: '', component:StoreComponent},
-  {path:'product/:id',component:ProductComponent}
+  {path: 'products', component:StoreComponent},
+  {path:'product/:id',component:ProductComponent},
+  {path: 'home', component:HomeComponent}
 ];
 
 @NgModule({
