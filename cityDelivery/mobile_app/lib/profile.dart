0bc2850011844d20def185d609e203bb21ba7cae
tiagomrm <59346879+tiagomrm@users.noreply.github.com>
2022-05-25 18:27:30 +0100

    added delivery requests and map

---
+++ cityDelivery/mobile_app/lib/profile.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+        body: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 60),
+            child: Column (
+              children: const [
+                Center(
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.deepPurple,
+                      size: 64.0,
+                    )
+                ),
+              ],
+            )
+        )
+    );
+  }
+
+}
---
+++ cityDelivery/mobile_app/lib/secrets.dart
@@ -0,0 +1,4 @@
+class Secrets {
+  // Add your Google Maps API Key here
+  static const apiKey = 'AIzaSyB2EBW8ZjUoayB13xPyl_VWBsJOnQtmZc4';
+}
