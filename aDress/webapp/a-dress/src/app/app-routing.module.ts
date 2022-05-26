878763578087e9610293106b1fb11ec3b916e816
AndreiaPp <andreia.portela@ua.pt>
2022-05-26 11:06:29 +0100

    added cart

--- aDress/webapp/a-dress/src/app/app-routing.module.ts
+++ aDress/webapp/a-dress/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product/product.component';
 import { StoreComponent } from './store/store.component';
 import { ProfileComponent } from './profile/profile.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
-  {path: 'products', component:StoreComponent},
+  {path:'products', component:StoreComponent},
   {path:'products/product/:id',component:ProductComponent},
   {path:'profile',component:ProfileComponent},
-  {path: '', component:HomeComponent}
+  {path:'cart',component:CartComponent},
+  {path:'', component:HomeComponent}
 ];
 
 @NgModule({
