f2718f326aac655f2382492f7d30ea7ee9bc45df
AndreiaPp <andreia.portela@ua.pt>
2022-06-09 14:56:56 +0100

    TQSPROJ-31 ignore cucumber

--- cucumbertest/src/test/java/cucumbertest/CucumberTest.java
+++ cucumbertest/src/test/java/cucumbertest/CucumberTest.java
@@ -9,7 +9,6 @@ import org.junit.platform.suite.api.Suite;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
-@Ignore
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("cucumbertest")
