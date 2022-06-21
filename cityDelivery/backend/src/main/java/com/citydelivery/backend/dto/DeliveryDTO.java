c7ed49a3e406c132a631dc1d4ebe9f10d19e1c9d
Tiago Matos <tiagomrm@ua.pt>
2022-06-21 23:55:17 +0100

    added DTOs

---
+++ cityDelivery/backend/src/main/java/com/citydelivery/backend/dto/DeliveryDTO.java
@@ -0,0 +1,60 @@
+package com.citydelivery.backend.dto;
+
+import java.time.LocalDateTime;
+
+public class DeliveryDTO {
+    private Long id;
+    private LocationDTO pickup;
+    private LocalDateTime pickupTime;
+    private LocationDTO dropOff;
+    private LocalDateTime deliveryTime;
+    private CourierDTO courier;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocationDTO getPickup() {
+        return pickup;
+    }
+
+    public void setPickup(LocationDTO pickup) {
+        this.pickup = pickup;
+    }
+
+    public LocalDateTime getPickupTime() {
+        return pickupTime;
+    }
+
+    public void setPickupTime(LocalDateTime pickupTime) {
+        this.pickupTime = pickupTime;
+    }
+
+    public LocationDTO getDropOff() {
+        return dropOff;
+    }
+
+    public void setDropOff(LocationDTO dropOff) {
+        this.dropOff = dropOff;
+    }
+
+    public LocalDateTime getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(LocalDateTime deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
+    public CourierDTO getCourier() {
+        return courier;
+    }
+
+    public void setCourier(CourierDTO courier) {
+        this.courier = courier;
+    }
+}
