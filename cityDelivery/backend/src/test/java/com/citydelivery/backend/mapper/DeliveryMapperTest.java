5b6643b7e27b21852c6fe492354c18e392e8de0e
Tiago Matos <tiagomrm@ua.pt>
2022-06-22 14:50:58 +0100

    added tests for mapping Lists

--- cityDelivery/backend/src/test/java/com/citydelivery/backend/mapper/DeliveryMapperTest.java
+++ cityDelivery/backend/src/test/java/com/citydelivery/backend/mapper/DeliveryMapperTest.java
@@ -5,7 +5,10 @@ import com.citydelivery.backend.model.Delivery;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DeliveryMapperTest {
@@ -37,5 +40,44 @@ class DeliveryMapperTest {
         assertEquals(deliveryDTO.getDeliveryTime(), delivery.getDeliveryTime());
     }
 
+
+    @Test
+    void whenConvertDeliveryDTOListToEntityList_thenCorrect() {
+        DeliveryDTO deliveryDTOA = new DeliveryDTO();
+        deliveryDTOA.setId(1L);
+        deliveryDTOA.setPickupTime(LocalDateTime.now());
+        deliveryDTOA.setDeliveryTime(LocalDateTime.now());
+
+        DeliveryDTO deliveryDTOB = new DeliveryDTO();
+        deliveryDTOB.setId(2L);
+        deliveryDTOB.setPickupTime(LocalDateTime.now());
+        deliveryDTOB.setDeliveryTime(LocalDateTime.now());
+
+        List<Delivery> mappedDeliveries = DeliveryMapper.INSTANCE.toDeliveries(Arrays.asList(deliveryDTOA, deliveryDTOB));
+
+        assertThat( mappedDeliveries.get(0).getId() ).isEqualTo(deliveryDTOA.getId());
+        assertThat( mappedDeliveries.get(0).getDeliveryTime() ).isEqualTo(deliveryDTOA.getDeliveryTime());
+        assertThat( mappedDeliveries.get(0).getPickupTime() ).isEqualTo(deliveryDTOA.getPickupTime());
+    }
+
+
+    @Test
+    void whenConvertDeliveryListToDTOList_thenCorrect() {
+        Delivery deliveryA = new Delivery();
+        deliveryA.setId(1L);
+        deliveryA.setPickupTime(LocalDateTime.now());
+        deliveryA.setDeliveryTime(LocalDateTime.now());
+
+        Delivery deliveryB = new Delivery();
+        deliveryB.setId(2L);
+        deliveryB.setPickupTime(LocalDateTime.now());
+        deliveryB.setDeliveryTime(LocalDateTime.now());
+
+        List<DeliveryDTO> mappedDeliveries = DeliveryMapper.INSTANCE.toDeliveryDTOs(Arrays.asList(deliveryA, deliveryB));
+
+        assertThat( mappedDeliveries.get(0).getId() ).isEqualTo(deliveryA.getId());
+        assertThat( mappedDeliveries.get(0).getDeliveryTime() ).isEqualTo(deliveryA.getDeliveryTime());
+        assertThat( mappedDeliveries.get(0).getPickupTime() ).isEqualTo(deliveryA.getPickupTime());
+    }
 
 }
