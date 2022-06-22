1753173bd56f1f8f48bac3612594aee05545f10f
tiagomrm <59346879+tiagomrm@users.noreply.github.com>
2022-06-23 00:14:28 +0100

    added validation of Delivery and body to `createDeliveryRequest`

--- cityDelivery/backend/src/main/java/com/citydelivery/backend/controller/DeliveryController.java
+++ cityDelivery/backend/src/main/java/com/citydelivery/backend/controller/DeliveryController.java
@@ -2,12 +2,17 @@ package com.citydelivery.backend.controller;
 
 import com.citydelivery.backend.dto.DeliveryDTO;
 import com.citydelivery.backend.dto.LocationDTO;
+import com.citydelivery.backend.mapper.DeliveryMapper;
+import com.citydelivery.backend.model.Delivery;
 import com.citydelivery.backend.service.DeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
@@ -18,8 +23,13 @@ public class DeliveryController {
     private DeliveryService deliveryService;
 
     @PostMapping
-    public ResponseEntity<DeliveryDTO> createDeliveryRequest(@RequestBody DeliveryDTO newDelivery) {
-        return ResponseEntity.accepted().build();
+    public ResponseEntity<DeliveryDTO> createDeliveryRequest(
+            @Valid @RequestBody DeliveryDTO newDeliveryDTO, BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors())
+            return ResponseEntity.badRequest().build();
+        Delivery newDelivery = DeliveryMapper.INSTANCE.toDelivery(newDeliveryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(deliveryService.save(newDelivery));
     }
 
     @GetMapping
